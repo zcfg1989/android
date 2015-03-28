3ff484dbe1f837664e98f6b2fc7746052368295e
zcfg1989 <zhangchaohz@foxmail.com>
2015-03-28 16:24:56 +0800

    添加第一个页面

--- News/src/com/example/news/MainTab01.java
+++ News/src/com/example/news/MainTab01.java
@@ -1,20 +1,40 @@
 package com.example.news;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.news.view.DialogFactory;
 
 public class MainTab01 extends Fragment
 {
+	private Context mContext;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		return inflater.inflate(R.layout.main_tab_01, container, false);
+		mContext = this.getActivity();
+		View myView = inflater.inflate(R.layout.main_tab_01, container, false);
+		TextView tv = (TextView)myView.findViewById(R.id.zhihu_tx);
+		Dialog loginingDialog = DialogFactory.getLoadingDialog((Activity) mContext,
+				"ÕýÔÚ¼ÓÔØ", true, null);
+		loginingDialog.setCanceledOnTouchOutside(false);
+		loginingDialog.show();
+		Loadhtml load = new Loadhtml(mContext,tv,loginingDialog);
+		load.execute();
+		return myView;
 		
 	}
+	
+
+
+
 	
 
 }
